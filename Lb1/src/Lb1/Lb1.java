8e81455c3d22e5e4531a891a77866cfda0d254ff
SoulLees <113638468+SoulLees@users.noreply.github.com>
2022-09-15 23:03:21 +0300

    Add files via upload

---
+++ Lb1/src/Lb1/Lb1.java
@@ -0,0 +1,15 @@
+package Lb1;
+
+import java.util.Random;
+
+public class Lb1 {
+    public static void main(String[] args) {
+        String Pri;
+        String Im;
+        String POB;
+        int age = 0;
+        double size = Math.random()*50-0;
+        System.out.println(size);
+
+    }
+}
